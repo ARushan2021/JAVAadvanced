d4561036281745d9822dabcec35b9b008d566651
ARushan2021 <93250178+ARushan2021@users.noreply.github.com>
2022-04-27 18:40:34 +0300

    Add files via upload

---
+++ Task1.java
@@ -0,0 +1,44 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+/**
+ * Task 1 from JAVA advanced
+ * @author Abdurahmanov Rushan
+ */
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите Аргумент 1: ");
+        double x = in.nextDouble();
+        System.out.println("Введите Аргумент 2: ");
+        double y = in.nextDouble();
+
+        Scanner in2 = new Scanner(System.in);
+        System.out.println("Введите действие '+', '-', '*', '/' : ");
+        String z = in2.nextLine();
+
+        in.close();
+        in2.close();
+
+        double result = 0;
+        String Otvet = "Ответ : " + x + " " + z + " " + y + " = ";
+
+        if (Objects.equals(z, "+")) {
+            result = x + y;
+            System.out.printf(Otvet + "%.4f", result);
+        } else if (Objects.equals(z, "-")) {
+            result = x - y;
+            System.out.printf(Otvet + "%.4f", result);
+        } else if (Objects.equals(z, "*")) {
+            result = x * y;
+            System.out.printf(Otvet + "%.4f", result);
+        } else if (Objects.equals(z, "/")) {
+            result = x / y;
+            System.out.printf(Otvet + "%.4f", result);
+        } else {
+            System.out.println("Действие введено не верно!!!");
+        }
+    }
+}
