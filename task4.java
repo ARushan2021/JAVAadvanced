35ee642a3f66cc66bbf08a93a0e02d9180b6a59c
ARushan2021 <93250178+ARushan2021@users.noreply.github.com>
2022-05-13 22:55:07 +0300

    замечания исправил

---
+++ task4.java
@@ -0,0 +1,13 @@
+import OOPCalculator.*;
+
+/**
+ * Task 2 from JAVA advanced
+ * @author Abdurahmanov Rushan
+ */
+
+public class task4 {
+        public static void main(String[] args) {
+            Calculator calc = new Calculator(10.46464, 2456, "-");
+            calc.Result();
+        }
+}
