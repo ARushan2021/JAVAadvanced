8ba522286c5f29020b456da452a4bd1d4ac12688
ARushan2021 <93250178+ARushan2021@users.noreply.github.com>
2022-05-14 18:36:36 +0300

    Add files via upload

---
+++ OOPCalculator/CalculatorTask6.java
@@ -0,0 +1,43 @@
+package OOPCalculator;
+
+public class CalculatorTask6 {
+
+    double results;
+    double argumentOne;
+    double argumentTwo;
+    String operation;
+
+    public CalculatorTask6( double argumentOne, double argumentTwo, String operation){
+        this.argumentOne = argumentOne;
+        this.argumentTwo = argumentTwo;
+        this.operation = operation;
+    }
+
+    public void Result () {
+
+        try{
+
+            switch (operation) {
+                case ("+"):
+                    results = argumentOne + argumentTwo;
+                    break;
+                case ("-"):
+                    results = argumentOne - argumentTwo;
+                    break;
+                case ("/"):
+                    if (argumentTwo == 0)
+                        throw new ArithmeticException();
+                    results = argumentOne / argumentTwo;
+                    break;
+                case ("*"):
+                    results = argumentOne * argumentTwo;
+                    break;
+                default:
+                    System.out.println("Действие введено не верно!!!");
+                    break;
+            }
+            System.out.printf("Ответ : " + argumentOne + " " + operation + " " + argumentTwo + " = " + "%.4f", results);
+        }
+        catch(ArithmeticException e){System.out.println("На ноль делить нельзя : " + e);}
+    }
+}
