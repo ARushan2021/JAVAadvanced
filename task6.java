6e4cea9c9d30cecb0f1479ed215ba39c2d35d246
ARushan2021 <93250178+ARushan2021@users.noreply.github.com>
2022-05-14 18:36:50 +0300

    Add files via upload

---
+++ task6.java
@@ -0,0 +1,13 @@
+import OOPCalculator.*;
+
+/**
+ * Task 6 from JAVA advanced
+ * @author Abdurahmanov Rushan
+ */
+
+public class task6 {
+    public static void main(String[] args) {
+        CalculatorTask6 calc = new CalculatorTask6(10, 1, "/");
+        calc.Result();
+    }
+}
