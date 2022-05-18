4142bfb65cbce60d0c07e0c8c39674428a274db5
ARushan2021 <93250178+ARushan2021@users.noreply.github.com>
2022-05-18 19:18:01 +0300

    Add files via upload

--- OOPCalculator/Calculator.java
+++ OOPCalculator/Calculator.java
@@ -11,20 +11,37 @@ public class Calculator {
         this.argumentOne = argumentOne;
         this.argumentTwo = argumentTwo;
         this.operation = operation;
+
     }
 
-    public void Result() {
 
-        switch (operation) {
-            case ("+"): results = argumentOne + argumentTwo; break;
-            case ("-"): results = argumentOne - argumentTwo; break;
-            case ("/"): results = argumentOne / argumentTwo; break;
-            case ("*"): results = argumentOne * argumentTwo; break;
-            default: System.out.println("Действие введено не верно!!!"); break;
+    public double Result() {
+
+            switch (operation) {
+                case ("+"):
+                    results = argumentOne + argumentTwo;
+                    break;
+                case ("-"):
+                    results = argumentOne - argumentTwo;
+                    break;
+                case ("/"):
+                    results = argumentOne / argumentTwo;
+                    break;
+                case ("*"):
+                    results = argumentOne * argumentTwo;
+                    break;
+                default:
+                    System.out.println("Действие введено не верно!!!");
+                    break;
             }
             System.out.printf("Ответ : " + argumentOne + " " + operation + " " + argumentTwo + " = " + "%.4f", results);
+        return results;
+        }
     }
-}
+
+
+
+
 
 
 
