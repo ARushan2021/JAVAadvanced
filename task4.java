06c836d59867840fdd2a99b96696e575c5b5c62d
ARushan2021 <93250178+ARushan2021@users.noreply.github.com>
2022-05-23 21:42:15 +0300

    Add files via upload

--- task4.java
+++ task4.java
@@ -1,13 +1,29 @@
 import OOPCalculator.*;
+import java.util.Scanner;
 
 /**
- * Task 4 from JAVA advanced
- * @author Abdurahmanov Rushan.
+ * Task 2 from JAVA advanced
+ * @author Abdurahmanov Rushan
  */
 
 public class task4 {
         public static void main(String[] args) {
-            Calculator calc = new Calculator(10.46464, 2456, "-");
+            //Если правильно понял, то нужно так:
+            //Ввод данных через консоль
+            Scanner in = new Scanner(System.in);
+            System.out.println("Введите Аргумент 1: ");
+            double x = in.nextDouble();
+            System.out.println("Введите Аргумент 2: ");
+            double y = in.nextDouble();
+            Scanner in2 = new Scanner(System.in);
+            System.out.println("Введите действие '+', '-', '*', '/' : ");
+            String z = in2.nextLine();
+            in.close();
+            in2.close();
+
+            //Передача введенных данных в калькулятор:
+            Calculator calc = new Calculator(x, y, z);
+            // Вывод результата в консоль:
             calc.Result();
         }
 }
