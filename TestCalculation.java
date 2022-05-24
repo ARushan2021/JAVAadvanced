dc9fdd23cdbee24a8ff20ef1288d903724c8075b
ARushan2021 <93250178+ARushan2021@users.noreply.github.com>
2022-05-24 23:08:41 +0300

    Add files via upload

--- TestCalculation.java
+++ TestCalculation.java
@@ -3,51 +3,44 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class TestCalculation {
-
+    //Проверка сложения, значение обоих аргументов положительное
     @Test
       public void TestCalculationPlusPositiveValues() {
           CalculatorTask6 calc = new CalculatorTask6(2.55555, 7.44445, "+");
           Assert.assertEquals(10.0000, calc.Result(), 0);
       }
-
+    //Проверка вычитания, значение обоих аргументов отрицательное
     @Test
     public void TestCalculationMinusNegativeValues() {
         CalculatorTask6 calc = new CalculatorTask6(-2.55555, -7.44445, "-");
         Assert.assertEquals(4.8889, calc.Result(), 0);
     }
-
+    //Проверка умножения, значение первого аргумента отрицательное, второго положительное
     @Test
     public void TestCalculationMultiplication() {
         CalculatorTask6 calc = new CalculatorTask6(-2.5, 10, "*");
         Assert.assertEquals(-25.0000, calc.Result(), 0);
     }
-
+    //Проверка деления, значение первого аргумента положительное, второго отрицательное
     @Test
     public void TestCalculationDivision() {
         CalculatorTask6 calc = new CalculatorTask6(888, -5, "/");
         Assert.assertEquals(-177.6000, calc.Result(), 0);
     }
-    //При "аргументе 1" = 10 и "аргументе 2" = 1, ожидаемый результат будет = 0, только если operation - невалидное значение.
-    @Test
-    public void TestCalculationNotValidAction () {
-        CalculatorTask6 calc = new CalculatorTask6(10, 1, "r");
-        Assert.assertEquals(0, calc.Result(), 0);
-    }
-
-    /*
-    "calc.Result();" -  возвращает переменую типа double.
-    При запуске теста получаю ошибку - "java.lang.AssertionError: Expected exception: java.lang.ArithmeticException"
-    (ожидаемый результат не исключение типа ArithmeticException)
-    Обойти ошибку к сожалению не получилось.
-    Если в тесте прописываю: int a = 10 / 0;, то тест отрабатывает кооректно
-    */
-
+    //Проверка исключения деления на ноль
     @Test (expected=ArithmeticException.class)
     public void TestDivisionByZero () {
         CalculatorTask6 calc = new CalculatorTask6(500, 0, "/");
         calc.Result();
     }
+    //Проверка невалидного значения операции
+    @Test (expected=IllegalArgumentException.class)
+    public void TestNoValidOperation () {
+        CalculatorTask6 calc = new CalculatorTask6(500, 10, "u");
+        calc.Result();
+    }
 }
+
 
 
 
