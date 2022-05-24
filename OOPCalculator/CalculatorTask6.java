a98f9d3a731b1bd3e9be54d88ebed6588ad2bc84
ARushan2021 <93250178+ARushan2021@users.noreply.github.com>
2022-05-24 23:07:30 +0300

    Add files via upload

--- OOPCalculator/CalculatorTask6.java
+++ OOPCalculator/CalculatorTask6.java
@@ -7,7 +7,6 @@ public class CalculatorTask6 {
     double argumentTwo;
     String operation;
 
-
     public CalculatorTask6(double argumentOne, double argumentTwo, String operation) {
         this.argumentOne = argumentOne;
         this.argumentTwo = argumentTwo;
@@ -15,30 +14,27 @@ public class CalculatorTask6 {
     }
 
     public double Result() {
-        try {
-            switch (operation) {
-                case ("+"):
-                    results = argumentOne + argumentTwo;
-                    break;
-                case ("-"):
-                    results = argumentOne - argumentTwo;
-                    break;
-                case ("/"):
-                    if (argumentTwo == 0)
-                        throw new ArithmeticException();
-                    results = argumentOne / argumentTwo;
-                    break;
-                case ("*"):
-                    results = argumentOne * argumentTwo;
-                    break;
-                default:
-                    System.out.println("Действие введено не верно!!!");
-                    break;
-            }
-            System.out.printf("Ответ : " + argumentOne + " " + operation + " " + argumentTwo + " = " + "%.4f", results);
-        } catch (ArithmeticException e) {
-            System.out.println("На ноль делить нельзя : " + e);
+
+        switch (operation) {
+            case ("+"):
+                results = argumentOne + argumentTwo;
+                break;
+            case ("-"):
+                results = argumentOne - argumentTwo;
+                break;
+            case ("/"):
+                if (argumentTwo == 0)
+                    throw new ArithmeticException();
+                results = argumentOne / argumentTwo;
+                break;
+            case ("*"):
+                results = argumentOne * argumentTwo;
+                break;
+            default:
+                throw new IllegalArgumentException();
         }
+        //Вывод ответа перенес в task6
+        //System.out.printf("Ответ : " + argumentOne + " " + operation + " " + argumentTwo + " = " + "%.4f", results);
        return results;
     }
 }
