b3544ec3b43d16c68620cf70dfd293c9d0f7fec4
ARushan2021 <93250178+ARushan2021@users.noreply.github.com>
2022-05-24 23:06:56 +0300

    Add files via upload

--- task6.java
+++ task6.java
@@ -1,5 +1,5 @@
 import OOPCalculator.*;
-
+import java.io.IOException;
 import java.util.Scanner;
 
 /**
@@ -8,19 +8,30 @@ import java.util.Scanner;
  */
 
 public class task6 {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Введите Аргумент 1: ");
-        double x = in.nextDouble();
-        System.out.println("Введите Аргумент 2: ");
-        double y = in.nextDouble();
-        Scanner in2 = new Scanner(System.in);
-        System.out.println("Введите действие '+', '-', '*', '/' : ");
-        String z = in2.nextLine();
-        in.close();
-        in2.close();
+    public static void main(String[] args) throws IOException {
+        try {
+            Scanner in = new Scanner(System.in);
+            System.out.println("Введите Аргумент 1: ");
+            double x = in.nextDouble();
+            System.out.println("Введите Аргумент 2: ");
+            double y = in.nextDouble();
+            Scanner in2 = new Scanner(System.in);
+            System.out.println("Введите действие '+', '-', '*', '/' : ");
+            String z = in2.nextLine();
+            in.close();
+            in2.close();
 
-        CalculatorTask6 calc = new CalculatorTask6(x, y, z);
-        calc.Result();
+            CalculatorTask6 calc = new CalculatorTask6(x, y, z);
+            System.out.printf("Ответ : " + x + " " + z + " " + y + " = " + "%.4f", calc.Result());
+        }
+        catch (ArithmeticException e) {
+            System.out.println("На ноль делить нельзя : " + e);
+            }
+        catch (IllegalArgumentException a) {
+                System.out.println("Действие введено не верно : " + a);
+            }
+        catch (Exception e) {
+            System.out.println("Введен некорректный Аргумент: " + e);
+        }
     }
 }
